68e9df800d662e514df3ac924f1de6814aaaa891
KangJekoo <peter6998@naver.com>
2023-05-15 04:09:02 +0900

    add _directory

---
+++ react_native_app/App.js
@@ -0,0 +1,30 @@
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './pages/home';
+import ChatScreen from './pages/chat';
+import VoiceScreen from './pages/voice';
+const Stack = createStackNavigator();
+
+export default function App() {
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+        <Stack.Screen name="Voice" component={VoiceScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ react_native_app/pages/voice.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { Button, Text, View } from 'react-native';
+import { Audio } from 'expo-av';
+import axios from 'axios';
+
+export default function VoiceScreen() {
+  const [recording, setRecording] = useState();
+  const [transcription, setTranscription] = useState('');
+
+  async function startRecording() {
+    try {
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      const recording = new Audio.Recording();
+      await recording.prepareToRecordAsync({
+        android: {
+          extension: '.m4a',
+          outputFormat: Audio.RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_MPEG_4,
+          audioEncoder: Audio.RECORDING_OPTION_ANDROID_AUDIO_ENCODER_AAC,
+          sampleRate: 48000,
+          numberOfChannels: 2,
+          bitRate: 192000,
+        },
+        ios: {
+          extension: '.m4a',
+          outputFormat: Audio.RECORDING_OPTION_IOS_OUTPUT_FORMAT_MPEG4AAC,
+          audioQuality: Audio.RECORDING_OPTION_IOS_AUDIO_QUALITY_MAX,
+          sampleRate: 48000,
+          numberOfChannels: 2,
+          bitRate: 192000,
+          linearPCMBitDepth: 16,
+          linearPCMIsBigEndian: false,
+          linearPCMIsFloat: false,
+        },
+      });
+      await recording.startAsync();
+      setRecording(recording);
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    const uri = recording.getURI();
+
+    const formData = new FormData();
+    formData.append('file', {
+      uri,
+      name: 'audio.m4a',
+      type: 'audio/m4a',
+    });
+
+    const response = await axios.post('http://<your_ip>:5000/transcribe', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    setTranscription(response.data.transcription);
+  }
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button
+        onPress={recording ? stopRecording : startRecording}
+        title={recording ? 'Stop Recording' : 'Start Recording'}
+      />
+      <Text>Transcription: {transcription}</Text>
+    </View>
+  );
+}
