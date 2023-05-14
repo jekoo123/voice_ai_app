197586dd00b2946c80118793fbac66b6e9de9965
KangJekoo <peter6998@naver.com>
2023-05-15 03:21:41 +0900

    Use_Google_Speech_to_Text

--- gpt.py
+++ gpt.py
@@ -1,37 +1,19 @@
 import os
 import openai
-import tempfile
+import os
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
-
+from pydub import AudioSegment
+from google.oauth2 import service_account
+from google.cloud import speech_v1p1beta1 as speech
 
 app = Flask(__name__)
 
-openai.api_key = "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"
-
-# @app.route('/transcribe', methods=['POST'])
-# def transcribe():
-#     if 'file' not in request.files:
-#         return jsonify({'error': 'No file provided'}), 400
-#     audio_file = request.files['file']
-#     headers = {
-#         'Content-Type': 'audio/x-wav',
-#         'Authorization': f'Bearer {openai.api_key}',
-#     }
-
-#     response = requests.post(
-#         'https://api.openai.com/v1/engines/whisper/asr',
-#         headers=headers,
-#         data=audio_file.read()
-#     )
-
-#     if response.status_code != 200:
-#         return jsonify({'error': 'Failed to transcribe audio'}), response.status_code
-
-#     transcription = response.json()['choices'][0]['text']
-#     return jsonify({'transcription': transcription})
-if not os.path.exists('uploads'):
-    os.makedirs('uploads')
+# OpenAI API 키 설정
+openai.api_key = "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"
+client_file = 'sa_speech_demo.json'
+credentials = service_account.Credentials.from_service_account_file(client_file)
+client = speech.SpeechClient(credentials=credentials)
 
 
 @app.route('/chat', methods=['POST'])
@@ -62,6 +44,9 @@ def chat():
         return jsonify({'error': str(e)}), 500
 
 
+if not os.path.exists('uploads'):
+    os.makedirs('uploads')
+
 @app.route('/transcribe', methods=['POST'])
 def transcribe_audio():
     if 'file' not in request.files:
@@ -69,38 +54,39 @@ def transcribe_audio():
 
     file = request.files['file']
     filename = secure_filename(file.filename)
+
+    if not filename.endswith(('.m4a', '.mp3', '.webm', '.mp4', '.mpga', '.wav', '.mpeg')):
+        return jsonify({"error": "Invalid file format. Supported formats: ['m4a', 'mp3', 'webm', 'mp4', 'mpga', 'wav', 'mpeg']"}), 400
+
     file_path = os.path.join("uploads", filename)
     file.save(file_path)
 
+    # Convert audio file to the required format
+    audio = AudioSegment.from_file(file_path)
+    audio = audio.set_frame_rate(48000)
+    audio = audio.set_channels(1)
+    audio = audio.set_sample_width(2)  # set sample width to 2
+    audio.export(file_path, format="wav")
 
-    # 오디오 파일을 WAV 형식으로 변환
-    with tempfile.NamedTemporaryFile(suffix=".wav") as wav_file:
-        with open(file_path, 'rb') as f:
-            files = {'file': f}
-            api_requestor = openai.api_requestor.APIRequestor()
-            response = api_requestor.request(
-                "POST",
-                "/v1/engines/text-davinci-002/audio/transcribe",
-                headers={
-                    "Content-Type": "multipart/form-data",
-                    "Transfer-Encoding": "chunked",
-                },
-                files=files,
-            )
-        # WAV 파일을 텍스트로 변환
-        with open(wav_file.name, 'rb') as f:
-            response = openai.Completion.create(
-                engine="text-davinci-002",
-                prompt=f"Transcribe the following audio file: {f.read()}",
-                max_tokens=2048,
-                n_stop=1,
-            )
+    with open(file_path, "rb") as audio_file:
+        content = audio_file.read()
+        audio = speech.RecognitionAudio(content=content)
 
-    # 변환된 텍스트 반환
-    transcription = response.choices[0].text.strip()
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=48000,
+        language_code="en-US",
+    )
+
+    response = client.recognize(config=config, audio=audio)
+
+    # Extract transcription results
+    for result in response.results:
+        transcription = result.alternatives[0].transcript
+
     return jsonify({"transcription": transcription})
 
-
-
 if __name__ == '__main__':
+    if not os.path.exists('uploads'):
+        os.makedirs('uploads')
     app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000), debug=True)
