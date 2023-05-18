8118d0bb803e53c244a82afb7cb4586e548fd28e
KangJekoo <peter6998@naver.com>
2023-05-18 13:15:31 +0900

    u_voice_func

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 import os
 import openai
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, send_file, jsonify
 from werkzeug.utils import secure_filename
 from pydub import AudioSegment
 from google.oauth2 import service_account
 from google.cloud import speech_v1p1beta1 as speech
+from google.cloud import texttospeech
+
 # from pymongo import MongoClient
 # from bson.objectid import ObjectId
 
@@ -13,7 +15,7 @@ from google.cloud import speech_v1p1beta1 as speech
 app = Flask(__name__)
 
 # OpenAI API 키 설정
-openai.api_key = "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"
+openai.api_key = "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"
 client_file = 'sa_speech_demo.json'
 credentials = service_account.Credentials.from_service_account_file(client_file)
 client = speech.SpeechClient(credentials=credentials)
@@ -89,6 +91,35 @@ def transcribe_audio():
         transcription = result.alternatives[0].transcript
 
     return jsonify({"transcription": transcription})
+
+
+@app.route('/tts', methods=['POST'])
+def synthesize_speech():
+    text = request.json.get('text')
+
+    client = texttospeech.TextToSpeechClient(credentials=credentials)
+
+    synthesis_input = texttospeech.SynthesisInput(text=text)
+
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US", 
+        ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    response = client.synthesize_speech(
+        input=synthesis_input, 
+        voice=voice, 
+        audio_config=audio_config
+    )
+
+    with open('output.mp3', 'wb') as out:
+        out.write(response.audio_content)
+
+    return send_file('output.mp3', mimetype='audio/mpeg')
 
 if __name__ == '__main__':
     if not os.path.exists('uploads'):
